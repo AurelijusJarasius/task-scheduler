8a96bab3551029a163410615d1a12a013f9f2d5d
Aurelijus Jarasius <aurelijusjarasius@gmail.com>
2024-07-19 22:24:04 +0000

    Add start message and consultant choice prompts

--- run.py
+++ run.py
@@ -12,8 +12,30 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('task_scheduler')
 
-consultant = SHEET.worksheet('consultant')
+def start():
+    """
+    Program start and welcome message
+    """
+    print("Program starting...\n")
+    print("Hey there! Welcome to the Task Scheduler!\n")
 
-consultant_data = consultant.get_all_values()
 
-print(consultant_data)
+def consultant_choice():
+    """
+    Display consultant list and get users choice
+    """
+    print("Choose your prefered consultant from the list\n")
+    consultant = SHEET.worksheet('consultant')
+    consultant_data = consultant.get_all_values()
+    print(consultant_data)
+
+    consultant_str = input("Select your consultant by First name:\n")
+    user_input = input(f"You have chosen {consultant_str} is this correct? (Y/N)")
+    if user_input.lower() == "y":
+        print("Great!")
+    else:
+        consultant_str = input("Select your consultant by First name:\n")
+        user_input = input(f"You have chosen {consultant_str} is this correct? (Y/N)")
+
+start()
+consultant_choice()
