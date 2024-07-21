7ed12e0783aa712fa50da925354dbfac685a67a5
Aurelijus Jarasius <aurelijusjarasius@gmail.com>
2024-07-21 22:27:52 +0000

    Add consultant choice input field and validation

--- run.py
+++ run.py
@@ -1,6 +1,17 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
+prog_start = """
+
+  _____                                        _____ _             _   _                   
+ |  __ \                                      / ____| |           | | (_)                  
+ | |__) | __ ___   __ _ _ __ __ _ _ __ ___   | (___ | |_ __ _ _ __| |_ _ _ __   __ _       
+ |  ___/ '__/ _ \ / _` | '__/ _` | '_ ` _ \   \___ \| __/ _` | '__| __| | '_ \ / _` |      
+ | |   | | | (_) | (_| | | | (_| | | | | | |  ____) | || (_| | |  | |_| | | | | (_| |_ _ _ 
+ |_|   |_|  \___/ \__, |_|  \__,_|_| |_| |_| |_____/ \__\__,_|_|   \__|_|_| |_|\__, (_|_|_)
+                   __/ |                                                        __/ |      
+                  |___/                                                        |___/       
+"""
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -12,30 +23,47 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('task_scheduler')
 
+
+
 def start():
     """
     Program start and welcome message
     """
-    print("Program starting...\n")
+    print(prog_start)
     print("Hey there! Welcome to the Task Scheduler!\n")
 
-
-def consultant_choice():
+def consultant_list():
     """
-    Display consultant list and get users choice
+    Display consultant list
     """
     print("Choose your prefered consultant from the list\n")
-    consultant = SHEET.worksheet('consultant')
+    consultant = SHEET.worksheet("consultant")
     consultant_data = consultant.get_all_values()
     print(consultant_data)
 
-    consultant_str = input("Select your consultant by First name:\n")
-    user_input = input(f"You have chosen {consultant_str} is this correct? (Y/N)")
-    if user_input.lower() == "y":
-        print("Great!")
-    else:
-        consultant_str = input("Select your consultant by First name:\n")
-        user_input = input(f"You have chosen {consultant_str} is this correct? (Y/N)")
+def consultant_choice():
+    """
+    User input to choose consultant from a fixed list
+    """
+    consultant_names = ["Johnny Bravo", "Homer Simpson", "Ned Flanders", "Peter Griffins", "Fred Flinstone"]
+
+    user_inputs = []
+
+    while True:
+        user_input = input(f"Select your consultant by their full name: ")
+    
+        if user_input in consultant_names:
+            user_inputs.append(user_input)
+            print(f"Great! You have chosen {user_input}")
+            break
+            return({user_input})
+        else:
+            print(f"Invalid input. Please enter a name from the list.")
+            break
+
 
 start()
+consultant_list()
 consultant_choice()
+
+
